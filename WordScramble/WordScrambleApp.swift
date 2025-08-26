8ebceb646003bc372f2c176b20690dc826dd069f
d.o.kononov <d.o.kononov@gmail.com>
2025-08-26 16:43:47 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dmitry Kononov on 26.08.25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
